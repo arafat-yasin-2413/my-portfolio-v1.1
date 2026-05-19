038f80481caa2e3355e3b8730815ad62dc4f1b15
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-19 21:38:58 +0600

    add new paragraph

--- src/components/general/navbar/Navbar.tsx
+++ src/components/general/navbar/Navbar.tsx
@@ -7,6 +7,7 @@ function Navbar() {
             <div>
                 {/* logo */}
                 <Logo></Logo>
+                <p className="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio similique ad, eligendi quo quas modi maxime nisi, laboriosam culpa totam necessitatibus minus corrupti laudantium soluta. Exercitationem corrupti quas est placeat quibusdam eos corporis illo nam nisi aliquid laborum dignissimos consequatur ipsam aut earum, harum quod blanditiis molestiae repellendus incidunt possimus!</p>
             </div>
         </nav>
     );
